20a3f3c1dae801ad15e2601c8e3e9772f52cb996
marco <ti.marco@publicacoesonline.com.br>
2017-11-27 07:23:23 -0200

    criptografica md5 na senha para realizar o log
    npm install crypto --save

--- app/models/usuariosDAO.js
+++ app/models/usuariosDAO.js
@@ -1,3 +1,6 @@
+var crypto = require("crypto");
+
+
 function usuariosDAO(connection){
     this._connection = connection();
 }
@@ -5,6 +8,11 @@ function usuariosDAO(connection){
 usuariosDAO.prototype.inserirUsuario = function(usuario){
     this._connection.open(function(err, mongoClient){
         mongoClient.collection("usuarios", function(err, collection){
+            
+            var senhaCriptogragada = crypto.createHash("md5").update(usuario.senha).digest("hex");
+            
+            usuario.senha = senhaCriptogragada;
+            
             collection.insert(usuario);
             
             mongoClient.close();
@@ -17,6 +25,10 @@ usuariosDAO.prototype.autenticar = function(dados, req, res){
     
     this._connection.open(function(err, mongoClient){
         mongoClient.collection("usuarios", function(err, collection){
+            
+            var senhaCriptogragada = crypto.createHash("md5").update(dados.senha).digest("hex");
+            dados.senha = senhaCriptogragada;
+            
             collection.find({usuario: dados.usuario, senha: dados.senha}).toArray(function(err, result){
                 
                 if (result[0] != undefined) {
